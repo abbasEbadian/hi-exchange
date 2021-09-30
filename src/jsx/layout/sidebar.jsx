9439a80a1fdc75f6be539acaa724d62c77ae38a3
abbas ebadian <abbasebadiann@gmail.com>
2021-09-30 18:25:16 +0330

    sidebar title persian fix

--- src/jsx/layout/sidebar.jsx
+++ src/jsx/layout/sidebar.jsx
@@ -16,29 +16,29 @@ function Sidebar() {
                 <div className="menu">
                     <ul>
                         <li>
-                            <Link to={"/"} data-toggle="tooltip" data-placement="right" title="Home">
+                            <Link to={"/"} data-toggle="tooltip" data-placement="right" title="اصلی">
                                 <span><i className="icofont-ui-home"></i></span>
                             </Link>
                         </li>
-                        <li><Link to={"/buy-sell"} data-toggle="tooltip" data-placement="right" title="Buy Sale">
+                        <li><Link to={"/buy-sell"} data-toggle="tooltip" data-placement="right" title="خرید و فروش">
                             <span><i className="icofont-stack-exchange"></i></span>
                         </Link>
                         </li>
-                        <li><Link to={"/accounts"} data-toggle="tooltip" data-placement="right" title="Accounts">
+                        <li><Link to={"/accounts"} data-toggle="tooltip" data-placement="right" title="حساب">
                             <span><i className="icofont-user-alt-4"></i></span>
                         </Link>
                         </li>
-                        <li><Link to={"/wallet"} data-toggle="tooltip" data-placement="right" title="Accounts">
+                        <li><Link to={"/wallet"} data-toggle="tooltip" data-placement="right" title="کیف پول">
                             <span><i className="icofont-wallet"></i></span>
                         </Link>
                         </li>
-                        <li><Link to={"/settings"} data-toggle="tooltip" data-placement="right" title="Settings">
+                        <li><Link to={"/settings"} data-toggle="tooltip" data-placement="right" title="تنظیمات">
                             <span><i className="icofont-ui-settings"></i></span>
                         </Link>
                         </li>
                         <li className="logout">
                             <button className="border-0 bg-transparent text-white fs-3" onClick={()=>dispatch(userLogout(history))} data-toggle="tooltip" data-placement="right"
-                                title="Signout">
+                                title="خروج">
                             <span><i className="icofont-power"></i></span>
                         </button>
                         </li>
