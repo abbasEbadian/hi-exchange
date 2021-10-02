8eed2c61906359e88d58be8368dc74b25569f6e8
abbas ebadian <abbasebadiann@gmail.com>
2021-10-02 13:33:06 +0330

    rm unnecessary info and files

--- proxy.js
+++ proxy.js
@@ -17,9 +17,7 @@ app.get('/cryptocurrency/*', (req,res) => {
     let url = `https://pro-api.coinmarketcap.com/v1${req.originalUrl}`
     axios.get(url,{headers: { 'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY }})
         .then(response => {
-            
-            console.log(response.data)
-            res.send(response.data)
+                        res.send(response.data)
         })
         .catch(err => {
             console.log(err)
@@ -27,30 +25,6 @@ app.get('/cryptocurrency/*', (req,res) => {
         })
 })
 
-app.post("/create_payment_link", (req , res)=>{
-
-    const url = process.env.IDPAY_GENERATE_LINK
-
-    const headers = {
-        "Content-Type": "application/json",
-        "X-API-KEY": process.env.IDPAY_TOKEN,
-        "X-SANDBOX": '0'
-    }
-    
-    const {order_id, amount, name, phone, mail, desc} = req.body
-    const data  =JSON.stringify({order_id, amount, callback:"https://hi-exchange.herokuapp.com/wallet"})
-    console.log({order_id, amount, name, phone, mail, desc, callback:"https://hi-exchange.herokuapp.com/wallet"});
-    
-    axios.post(url, data, {headers}).then(response=>{
-        const {data} = response
-        res.send(data)
-    }).catch(error=>{
-        // console.log(error);
-        
-        res.send(error)
-    })
-
-})
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log('Listening on port ',port)
