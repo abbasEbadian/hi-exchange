f4fdb74b411783c5e8bb787cf8e0c3c4982fbba5
abbas ebadian <abbasebadiann@gmail.com>
2021-10-20 13:54:16 +0330

    proxy

--- proxy.js
+++ proxy.js
@@ -26,6 +26,6 @@ app.get('/cryptocurrency/*', (req,res) => {
 })
 
 const port = process.env.PORT || 5000
-app.listen(port, () => {
+app.listen(port, '127.0.0.1', () => {
     console.log('Listening on port ',port)
 })
