6bd4ecc2ce98adb0fc220dcbcb0ce0e0c4cbac74
abbas ebadian <abbasebadiann@gmail.com>
2021-12-23 22:51:37 +0330

    chart

--- src/jsx/charts/IRTChart.jsx
+++ src/jsx/charts/IRTChart.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactApexChart from "react-apexcharts";
 import axios from 'axios'
 import Loader from 'react-loader-spinner'
-function IRTChart({currency}) {
+import {useSelector} from 'react-redux'
+function IRTChart({}) {
     const [series, setSeries] = React.useState({})
     const [categories, setCategories] = React.useState([])
     const [time, setTime] = React.useState(undefined)
-    const [currency2, setCurrency2] = React.useState(currency)
+    const chart = useSelector(state=>state.wallet.chart)
     const options = {
-         chart: {
+        chart: {
           height: 350,
           type: 'line',
           zoom: {
@@ -22,9 +23,11 @@ function IRTChart({currency}) {
         grid: {
           row: {
             colors: ['transparent', 'transparent'], // takes an array which will be repeated on columns
-            opacity: 0.5
+            opacity: 0.2
           },
         },
+        
+        
         xaxis: {
           type: 'datetime',
           categories
@@ -33,11 +36,14 @@ function IRTChart({currency}) {
           labels:{
              style: {
               colors: ["white"],
-              fontSize: '12px',
-              fontFamily: 'Helvetica, Arial, sans-serif',
+              fontSize: '14px',
+              fontFamily: 'farsi-numbers, IranYekan, roboto, Arial',
               fontWeight: 400,
               cssClass: 'apexcharts-xaxis-label',
             },
+            formatter: (x,y)=>{
+              return Number(x).toLocaleString('fa-IR')
+            }
           }
         },
         tooltip: {
@@ -48,13 +54,13 @@ function IRTChart({currency}) {
       }
 
 
-    const get_latest = ()=>{
-      axios.get(`http://162.55.11.144:8000/api/v1/historical/?s=${currency2}`)
+    const get_latest = (currency)=>{
+      axios.get(`http://162.55.11.144:8000/api/v1/historical/?s=${chart}`)
         .then(response=>{
             const {data} = response
             
             setSeries([
-                {name: currency2+"-IRT", data:data.map(i=>{return +i["price"].toFixed()})},
+                {name: currency+"-IRT", data:data.map(i=>{return (+i["price"]).toFixed()})},
             ])
             setCategories(data.map(i=>{let a = new Date(+i["timestamp"]*1000); return a.toLocaleString()}))
         })
@@ -62,21 +68,17 @@ function IRTChart({currency}) {
     }
     if(!time){
       const t = setTimeout(() => {
-        get_latest(currency)
+        get_latest(chart || "BTC")
       }, 60000)
       setTime(t)
     }
     React.useEffect(()=>{
-      setCurrency2(currency)
-        if(!currency)return
-        get_latest()
-
-    }, [currency])
-    console.log(series, categories);
+      get_latest(chart || "BTC")
+    },[chart])
     
     return (
         <>
-        {series.length? 
+        {series.length? <>
         <div id="chart-timeline">
           <ReactApexChart
             options={options}
@@ -85,9 +87,13 @@ function IRTChart({currency}) {
             height={350}
           />
         </div>
+        <small className="mt-3 d-block text-end" dir="rtl">آخرین قیمت:
+         <span className="text-primary">{Number(series[0].data[0]).toLocaleString("fa-IR")}</span>
+        <small style={{fontSize: "12px"}}>{" "} تومان { " " }(یک دقیقه پیش)</small>
+         </small>
+        </>
         :
-        <div className="w-100 text-center"><Loader type={"Circles"} height={45}/></div>
-        }
+        <div className="w-100 text-center pt-5"><Loader type={"Circles"} height={45}/></div>}
         </>
     )
 }
